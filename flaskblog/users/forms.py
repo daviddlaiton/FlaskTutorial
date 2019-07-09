d360640d4243169727423797857eae897ad023ec
daviddlaiton <ad.laiton10@uniandes.edu.co>
2019-07-08 22:17:29 -0500

    All working until custom error pages

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
-from wtforms import StringField,PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
@@ -35,18 +35,25 @@ class UpdateAccountForm(FlaskForm):
     submit = SubmitField("Update")
     
     def validate_username(self, username):
-        if username.data != current_user.username:
-            user = User.query.filter_by(username=username.data).first()
-            if user:
-                raise ValidationError("Username already taken.") 
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username already taken.") 
 
     def validate_email(self, email):
-        if email.data != current_user.email:
-            user = User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError("Email already used.") 
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email already used.") 
 
-class PostForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired()])
-    content = TextAreaField("Content", validators=[DataRequired()])
-    submit = SubmitField("Post")
+class RequestResetForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Request password reset")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError("There is no account with that email.") 
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(" Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Reset password")
